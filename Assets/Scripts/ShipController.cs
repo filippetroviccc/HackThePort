ff3df976618aa3f74d03adfc152c2d8d0bdb0a67
Mihajlo Stefanovic <47528779+MihajloStefanovicL@users.noreply.github.com>
2019-05-23 11:58:16 +0200

    Made ship movement

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -1,19 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ShipController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float movementSpeed;
+    public float rotationSpeed;
 
-    // Update is called once per frame
+    public Rigidbody2D shipBody;
+
     void Update()
     {
-        
+        void HandleMovement()
+        {
+            var leftRotation = 1;
+            var rightRotation = -1;
+
+            void Move(Vector2 dir)
+            {
+                shipBody.AddForce(dir * movementSpeed);
+            }
+
+            void Rotate(int dir)
+            {
+                shipBody.angularVelocity += dir * rotationSpeed;
+            }
+
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) Move(transform.up);
+            else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) Move(-transform.up);
+
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) Rotate(leftRotation);
+            else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) Rotate(rightRotation);
+        }
+
+        HandleMovement();
     }
 
     private void HandleInput()
