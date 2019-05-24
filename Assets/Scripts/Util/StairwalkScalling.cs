f6131490ea8d06189f66cddb127166fe275f79dd
Данило Хаџи-Пешић <danilo.hadzi.pesic@gmail.com>
2019-05-24 11:02:59 +0200

    Stairwalk scaling work in progress

---
+++ Assets/Scripts/Util/StairwalkScalling.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StairwalkScalling : MonoBehaviour
+{
+    public struct Trespasser
+    {
+        public GameObject go;
+        public Vector3 previousPosition;
+    }
+    public float scaleFactor;
+    bool leftToRight;
+
+    private Dictionary<GameObject, Trespasser> trespassers;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(Trespasser t in trespassers.Values)
+        {
+            //t.go.transform.localScale =
+            
+        }
+    }
+
+    public void trespasserDetected(GameObject trespasser, bool walkingUp)
+    {
+        if(trespassers.ContainsKey(trespasser))
+        {
+            
+        } else
+        {
+            //trespassers[trespasser] = 
+        }
+    }
+}
