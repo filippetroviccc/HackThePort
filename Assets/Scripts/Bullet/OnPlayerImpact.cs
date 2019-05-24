56477949f63cc8ec2b45998bc15a16b0945e0182
Mihajlo Stefanovic <47528779+MihajloStefanovicL@users.noreply.github.com>
2019-05-24 06:38:19 +0200

    Implemented second enemy. + camera improvements

---
+++ Assets/Scripts/Bullet/OnPlayerImpact.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class OnPlayerImpact : MonoBehaviour
+{
+    public int damage;
+
+    private void Start()
+    {
+        StartCoroutine(DestroyThisObject());
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var player = other.gameObject.GetComponent<PlayerHealth>();
+        if (player != null) player.TakeDamage(damage);
+
+        if (!other.gameObject.CompareTag("Enemy") && !other.isTrigger) Destroy(gameObject);
+    }
+
+    private IEnumerator DestroyThisObject()
+    {
+        yield return new WaitForSeconds(2);
+        Destroy(gameObject);
+    }
+}
